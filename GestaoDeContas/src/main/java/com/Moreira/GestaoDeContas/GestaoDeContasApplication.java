8f70ad82bb4fc656cc82149134f7ded059adce92
JoaoVMoreira <jvittormoreira@outlook.pt>
2023-09-13 02:18:03 -0300

    Primeiro Commit

---
+++ GestaoDeContas/src/main/java/com/Moreira/GestaoDeContas/GestaoDeContasApplication.java
@@ -0,0 +1,13 @@
+package com.Moreira.GestaoDeContas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeContasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeContasApplication.class, args);
+	}
+
+}
---
+++ GestaoDeContas/src/test/java/com/Moreira/GestaoDeContas/GestaoDeContasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Moreira.GestaoDeContas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeContasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
