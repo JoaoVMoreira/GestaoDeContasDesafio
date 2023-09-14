12f0618044ffcac89fcf6c9b6d0d4f4edbf4ff86
JoaoVMoreira <jvittormoreira@outlook.pt>
2023-09-13 23:19:58 -0300

    Criando pastas do projeto e configs iniciais

---
+++ GestaoDeContas/src/main/java/com/Moreira/GestaoDeContas/Models/Conta.java
@@ -0,0 +1,15 @@
+package com.Moreira.GestaoDeContas.Models;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "contas")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Conta {
+}
